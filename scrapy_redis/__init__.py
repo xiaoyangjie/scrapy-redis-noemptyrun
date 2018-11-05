3652335a01669d06ad083912cdf1a86def830df6
wangshuang <15736169609@163.com>
2018-11-05 14:55:39 +0800

    scrapy-redis-noemptyrun1.0

---
+++ scrapy_redis/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from .connection import (  # NOQA
+    get_redis,
+    get_redis_from_settings,
+)
+
+
+__author__ = 'yangyuya'
+__email__ = '1019177406@qq.com'
+__version__ = '1.0'
---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+#############################################
+# File Name: setup.py
+# Author: yangyuya
+# Mail: 1019177406@qq.com
+# Created Time:  2018-11-5 13:58:00
+#############################################
+
+from setuptools import setup, find_packages            
+
+setup(
+    name="scrapy-redis-noemptyrun",
+    version="1.0",
+    keywords=("pip", "scrapy-redis-noemptyrun","featureextraction"),
+    description="deal with scrapy-redis's the problem of empty run",
+    long_description="",
+    license="MIT Licence",
+    url="https://github.com/xiaoyangjie/scrapy-redis-noemptyrun",
+    author="yangyuya",
+    author_email="1019177406@qq.com",
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["Scrapy>=1.0", "redis>=2.10",
+                      "six>=1.5.2"]
+)
